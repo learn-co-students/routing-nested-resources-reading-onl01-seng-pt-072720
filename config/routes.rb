81035ede24ba5dccf5b5dd6ef426adc7d6914e7e
sergiocardenasg <sergiocardenas@utexas.edu>
2020-12-23 20:59:54 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
+  resources :authors do
+    # nested resource for posts
+    resources :posts
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
