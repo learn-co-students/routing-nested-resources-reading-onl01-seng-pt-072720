37c9c18a87ea746a71477577271c49bd4f1b01df
OmarMontemayor <omarmontemayor11@yahoo.com>
2021-01-11 19:41:50 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,23 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      @post = Author.find(params[:author_id]).posts.find(params[:id])
+    else
+      @post = Post.find(params[:id])
+    end
   end
 
   def new
-    @post = Post.new
+    @post = Post.new(author_id: params[:author_id])
   end
 
   def create
@@ -25,12 +33,24 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    def edit
+      if params[:author_id]
+        author = Author.find_by(id: params[:author_id])
+        if author.nil?
+          redirect_to authors_path, alert: "Author not found."
+        else
+          @post = author.posts.find_by(id: params[:id])
+          redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+        end
+      else
+        @post = Post.find(params[:id])
+      end
+    end
   end
 
-private
+  private
 
   def post_params
-    params.require(:post).permit(:title, :desription, :post_status, :author_id)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
